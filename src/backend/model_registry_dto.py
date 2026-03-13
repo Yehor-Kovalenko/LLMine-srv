6fbf1c47875d749094c920d7c5e0a8185d2e4d43
Yehor <ka.egor.ka@gmail.com>
2026-03-13 21:05:57 +0100

    some additions to the modelpackage dto that is being sent to the engines and enginefactory

--- src/backend/model_registry_dto.py
+++ src/backend/model_registry_dto.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+from typing import Optional
 
 
 class ModelFormat(Enum):
@@ -10,7 +11,13 @@ class ModelFormat(Enum):
 
 @dataclass
 class ModelPackage:
+    id: str # unique model id ("meta/ollama3:4b" for example)
+    model_type: str # general type (classifier, llm, embedder etc.)
+
     path: str # path to the downloaded model
     format: ModelFormat # format of the model
-    model_type: str # general type (classifier, llm, embedder etc.)
+
     metadata: dict #additional useful metadata
+    architecture: str = "" # architecture llama, bert, mistral etc.
+    context_length: int = 2048
+    dimensions: Optional[int] = None # dimensions of the embeddings
